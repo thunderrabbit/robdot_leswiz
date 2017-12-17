e31be7b75cbb0dda9147e993ef907e61eb16d0f5
Rob Nugen <rob@robnugen.com>
2017-12-17 06:23:59 -0200

    Prove concept of icons

--- subscenes/GameSprite.gd
+++ subscenes/GameSprite.gd
@@ -8,8 +8,14 @@ var colorset = [
 	Color(0.484998, 0.480191, 0.932654)
 ]
 
+##  these hardcoded items should be folded into item_database.gd
+##  but I cannot seem to get it loaded here
+##  so for now, just hardcode them.
 var ICON_SIZE = 50   ## made up shit
 var RAW_LENGTH = 60  # mad up shit
+func get_size():
+	return Vector2(50,60)   ### Made up shit should be based on global / item_database
+
 var ItemDatabase		# Will know about pieces   I want this to be a singleton outside this Class
 
 func _ready():
@@ -18,13 +24,11 @@ func _ready():
 	ItemDatabase = get_node("/root/item_database")
 	pass
 
-func get_size():
-	return Vector2(50,60)   ### Made up shit tyring to get hit sot owrk
-
+# Change my_type_ordinal to get different icons
 func set_type_ordinal(my_type_ordinal):
 	# set the color
-# use icon not color	set_modulate(colorset[my_type_ordinal])
-	var icon = 0   # Fack figure out Database later	ItemDatabase.get_item_sprite(my_type_ordinal)
+    # use icon not color	set_modulate(colorset[my_type_ordinal])
+	var icon = my_type_ordinal   # Fack figure out Database later	ItemDatabase.get_item_sprite(my_type_ordinal)
 	set_pos(get_size()/2)
 	set_scale(get_size()/ICON_SIZE)
 	set_texture(preload("res://items.png"))
